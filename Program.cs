20078025701ac7481922cdbf040b21d7d6410afc
Andrew Kanieski <emailme@andrewkanieski.com>
2020-12-22 13:01:36 -0500

    Simple captcha generator

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace captcha_sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var code = Captcha.GenerateCaptchaCode();
+            Console.WriteLine($"Code: {code}");
+            var result = Captcha.GenerateCaptchaImage(300,100, code);
+            System.IO.File.WriteAllBytes($"{result.CaptchaCode}.png", result.CaptchaByteData);
+
+        }
+    }
+}
